61f1ef85eb512be10c18330cd1e4d7d8026ea929
rupalimarwadi <rupali0103@gmail.com>
2020-11-28 20:48:54 +0530

    Firebase crud demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentListComponent } from './student-list/student-list.component';
 
 const routes: Routes = [];
 
@@ -7,4 +8,6 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCckT58XLS8m6L60hLeToMKS6UZR-GKknU",
+    authDomain: "fir-demo-cebb5.firebaseapp.com",
+    databaseURL: "https://fir-demo-cebb5.firebaseio.com",
+    projectId: "fir-demo-cebb5",
+    storageBucket: "fir-demo-cebb5.appspot.com",
+    messagingSenderId: "459973876722",
+    appId: "1:459973876722:web:5de7613e8237e97abbe96b",
+    measurementId: "G-600FRYBD5E"
+  }
 };
 
 /*
